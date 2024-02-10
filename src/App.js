5a72b3552e70837b1912dbb67be376c17f64c08b
viraj812 <toviraj08@gmail.com>
2024-02-10 13:36:50 +0530

    create

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
+import profile from './profile_pic.png'
 import './App.css';
+import { useEffect, useState } from 'react';
 
-function App() {
+function NavComponent() {
+  return (<nav>
+    <div className='titleDiv'>Portfolio</div>
+
+    <div className='navButtons'>
+      <div className='nav-item'>Home</div>
+      <div className='nav-item'>Skills</div>
+      <div className='nav-item'>About</div>
+      <div className='nav-item'>Contact</div>
+    </div>
+  </nav>);
+}
+
+
+const TypewriterAnimation = (props) => {
+  const [currentText, setCurrentText] = useState('');
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+
+  useEffect(() => {
+    if (currentIndex < props.text.length) {
+      let txtArray = Array.from(props.text);
+      setTimeout(() => {
+        setCurrentText(currentText + txtArray[currentIndex]);
+        setCurrentIndex(currentIndex + 1);
+      }, props.delay)
+    }
+  }, [currentText]);
+
+
+  return <span>{currentText}</span>;
+};
+
+const ContentBoxComponent = () => {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='contentBox'>
+      <div className='contentDiv'>
+        <h2><span className='pronoun-span'>Hi There, <br />
+          I'm </span>
+          <span className='name-span'><TypewriterAnimation text="VIRAJ DESAI" delay={100} /></span>
+        </h2>
+
+        <p className='role'><span className='role-span'> Full-Stack Developer</span></p>
+      </div>
+
+      <img src={profile} alt='profile picture' />
     </div>
   );
+}
+
+const mainPage = (
+  <div className='mainDiv'>
+
+    <NavComponent />
+
+    <div className='pageDiv'>
+      <ContentBoxComponent />
+    </div>
+  </div>
+);
+
+function App() {
+  return mainPage;
 }
 
 export default App;
