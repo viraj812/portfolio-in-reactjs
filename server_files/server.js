e7cb9f0a4325b3b5b0e25f1785e92fea79f02575
viraj812 <toviraj08@gmail.com>
2024-02-13 00:19:39 +0530

    update

--- server_files/server.js
+++ server_files/server.js
@@ -5,11 +5,12 @@ const app = new express();
 app.use(express.json());
 
 app.use(express.static('client'));
+// app.use(express.static('client/mobile-build'));
 // app.use(express.static('client/desktop-build'));
 
 app.get('*', (req, res) => {
     if (req.header('user-agent').indexOf('Mobile') != -1) {
-        return res.sendFile(path.resolve(__dirname, 'client', 'desktop-build', 'index.html'));
+        return res.sendFile('index.html');
     }
     else {
         return res.sendFile(path.resolve(__dirname, 'client', 'mobile-build', 'index.html'));
