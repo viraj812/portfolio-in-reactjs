b06ea219268d7099a119bae514809e50a268b5df
viraj812 <toviraj08@gmail.com>
2024-02-12 23:32:39 +0530

    "added build to server files"

--- server_files/server.js
+++ server_files/server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const app = new express();
 app.use(express.json());
 
-app.use(express.static('client/'));
+app.use(express.static('./client/'));
 
 app.get('*', (req, res) => {
     if (req.header('user-agent').indexOf('Mobile') != -1) {
