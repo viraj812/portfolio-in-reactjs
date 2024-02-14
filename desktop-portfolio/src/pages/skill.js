f888052c1470a711e2945210a6a87c13ba6a4350
viraj812 <toviraj08@gmail.com>
2024-02-15 01:04:40 +0530

    skill update

--- desktop-portfolio/src/pages/skill.js
+++ desktop-portfolio/src/pages/skill.js
@@ -3,7 +3,21 @@ const SkillPage = () => {
         <div className="mainDiv">
             <div className="pageDiv">
                 <div className="contentBox">
-                    
+                    <div className="skills-div">
+                        <div className="head-div">
+                            <h2>Skills</h2>
+                        </div>
+
+                        <div className="body-div">
+                            <ul>
+                                <li>Full Stack Development</li>
+                                <li>Android App Development</li>
+                                <li>Database</li>
+                                <li>Data Science and Analysis</li>
+                                <li>Artificial Intelligence and Machine Learning</li>
+                            </ul>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
