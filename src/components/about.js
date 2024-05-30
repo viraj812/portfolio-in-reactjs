257b6f613ca50f0fb20ae80a36561e565550760c
viraj812 <toviraj08@gmail.com>
2024-05-30 18:45:35 +0530

    major updates

--- src/components/about.js
+++ src/components/about.js
@@ -1,4 +1,4 @@
-import profile_pic from "../vj.png"
+import profile_pic from "../about-pic.jpg"
 
 const AboutPage = (props) => {
     return (
@@ -13,34 +13,36 @@ const AboutPage = (props) => {
                         <img src={profile_pic} />
                     </div>
                     <div className='about1-content'>
-                        <p><span className='info'>Name:</span> Viraj Desai</p>
-                        <p><span className='info'>Qualification:</span> BTech Computer Science</p>
-                        <p><span className='info'>Experience:</span> Fresher</p>
-                        <p><span className='info'>Interest:</span> Artificial Intelligence, Data Science, Full Stack Development</p>
-                        <p><span className='info'>Location:</span> Ahmedabad, Gujarat</p>
+                        <p><span className='info'>Name :-</span><br />Viraj Desai</p>
+                        <span className='info'>Qualification :-</span>BTech Computer Science Engineering
+                        <p><span className='info'>Experience :-</span><br />Fresher</p>
+                        <span className='info'>Interest :-</span>Artificial Intelligence, Data Science, Full Stack Development
+                        <p><span className='info'>Location :-</span><br /> Ahmedabad, Gujarat, India</p>
                     </div>
                 </div>
 
                 <div className='about2'>
                     <div className='about2-content'>
                         I'm Viraj Desai, an enthusiastic Computer Science Engineering student with a strong inclination towards utilizing technology for addressing real-world challenges. Presently enrolled in my Bachelor's program at Pandit Deendayal Energy University, Gandhinagar, I am captivated by the constantly evolving realm of computer science.
+                        <br /><br />
+                        Educational Background :-
                         <br />
-                        Educational Background:
                         My academic journey has endowed me with a robust grasp of fundamental computer science principles, encompassing algorithms, data structures, programming languages, and software engineering tenets. I have consistently pursued academic excellence, maintaining a commendable GPA while actively participating in extracurricular pursuits that complement my studies.
 
                         <br /><br />
-                        Technical Proficiency:
+                        Technical Proficiency :-
                         <br />
                         <ul>
-                            <li>Proficiency in programming languages such as Java, Python, C++, and JavaScript.</li>
+                            <li>Proficiency in programming languages such as Python, JavaScript, Kotlin, Java, C and C++.</li>
                             <br />
-                            <li>Hands-on experience in crafting web applications using HTML, CSS, and diverse frontend and backend frameworks.<br /></li>
+                            <li>Hands-on experience in crafting full stack web applications using HTML, CSS, and frameworks such as ReactJS, NodeJS, Flask, Django.<br /></li>
+                            <br />
+                            <li>Strong fundamentals and practical knowledge, skills in machine learning and artificial intelligence, demonstrated through practical implementation of algorithms and models.</li>
                             <br />
                             <li>Proficient in managing database systems including MySQL, MongoDB, and PostgreSQL.</li>
                             <br />
-                            <li>Acquaintance with cloud computing platforms like AWS and Azure, coupled with practical expertise in application deployment and management.</li>
-                            <br />
-                            <li>Familiarity with machine learning and artificial intelligence principles, demonstrated through practical implementation of algorithms and models.</li>
+                            <li>Practical expertise in cloud services such as AWS and application deployment and management.</li>
+
                         </ul>
                     </div>
                 </div>
